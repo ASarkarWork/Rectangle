6b2751cb2709088f2284a54cefb8719bcf830ce8
Atri  Sarkar <atrisarkar.work@gmail.com>
2023-07-07 13:47:24 +0000

    Add. support for fractional length and breadth

--- src/main/java/com/thoughtworks/rectangle/Rectangle.java
+++ src/main/java/com/thoughtworks/rectangle/Rectangle.java
@@ -1,13 +1,16 @@
 package com.thoughtworks.rectangle;
 
 class Rectangle {
-    private final int length, breadth;
+    private final double length, breadth;
 
-    public Rectangle(int length, int breadth) {
+    public Rectangle(double length, double breadth) {
         this.length = length;
         this.breadth = breadth;
     }
-    public int area() {
+    public double area() {
         return length * breadth;
+    }
+    public double perimeter() {
+        return 2 * (length + breadth);
     }
 }
--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -3,12 +3,17 @@ package com.thoughtworks.rectangle;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
-
 class RectangleTest {
     @Test
     public void shouldReturnAreaWhenLengthAndBreadthAreGiven() {
-        var rectangle = new Rectangle(4, 5);
-        int area = rectangle.area();
-        assertThat(area, is(equalTo(20)));
+        var rectangle = new Rectangle(3.14, 2.67);
+        double area = rectangle.area();
+        assertThat(area, is(closeTo(8.3838, 0.01)));
+    }
+    @Test
+    public void shouldReturnPerimeterWhenLengthAndBreadthAreGiven() {
+        var rectangle = new Rectangle(3.14, 2.67);
+        double perimeter = rectangle.perimeter();
+        assertThat(perimeter, is(closeTo(11.62, 0.01)));
     }
 }
